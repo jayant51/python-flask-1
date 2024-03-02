4aaacce856e538de6c0c524f8e5bb67ea3963061
jayc51 <jayant.kulkarni@ibm.com>
2024-03-01 23:10:02 -0800

    first commit

--- app.py
+++ app.py
@@ -12,12 +12,46 @@ def hello():
 @app.route("/res", methods=["GET"])
 def res():
     if request.method == "GET":
-        data = {
-            "ponum": 15,
-            "lineitem": "keyboard",
+        """
+        data = {"ponum": 15, "lineitem": "keyboard"}
+        """
+        podata = {
+            "OrderLines": {
+                "OrderLine": {
+                    "CarrierServiceCode": "UPS",
+                    "ManufacturerName": "",
+                    "DeliveryMethod": "SHP",
+                    "RecevingNode": "",
+                    "DeliveryCode": "Collect",
+                    "ReqDeliveryDate": "2024-02-09",
+                    "PrimeLineNo": "1",
+                    "CustomerPONo": "C123456-4",
+                    "Item": {
+                        "CustomerItem": "",
+                        "UnitOfMeasure": "EACH",
+                        "ItemDesc": "Laptop 14",
+                        "ManufacturerItem": "",
+                        "ProductClass": "",
+                        "ItemID": "Laptop 14",
+                    },
+                    "CarrierAccountNo": "123",
+                    "CustomerLinePONo": "1",
+                    "SubLineNo": "1",
+                    "FillQuantity": "",
+                    "SCAC": "UPS",
+                    "ShipmentConsolidationGroupId": "",
+                    "ItemGroupCode": "",
+                    "ReqShipDate": "2024-02-20",
+                    "LinePriceInfo": {"UnitPrice": "0", "IsPriceLocked": "Y"},
+                    "ShipNode": "ATOS_Store",
+                    "FreightTerms": "",
+                    "OrderedQty": "1",
+                    "LineType": "DS",
+                    "FulfillmentType": "SHP",
+                }
+            }
         }
-
-        return jsonify(data)
+        return jsonify(podata)
 
 
 if __name__ == "__main__":
